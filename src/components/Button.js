6b9d79e44eed610d813cc0c1dae901082542d797
knoori-code <knoori3@gmail.com>
2023-02-21 16:46:50 -0500

    Complete creating button which conditionally renders styles based on state

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,26 @@
 import React from "react";
-
 import "components/Button.scss";
+import classNames from "classnames";
+
+// Base uses no props and is considered the default button
+// Confirm uses the confirm prop to apply the .button--confirm modifier class
+// Danger uses the danger prop to apply the .button--danger modifier class
+// Clickable uses the onClick prop to handle the button click event
+// Disabled uses the disabled prop to apply the disabled attribute to the button element
 
 export default function Button(props) {
-   return <></>;
+  const buttonClass = classNames("button", {
+    "button--confirm": props.confirm,
+    "button--danger": props.danger
+  });
+
+  return (
+    <button
+      className={buttonClass}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
+      {props.children}
+    </button>
+  );
 }
