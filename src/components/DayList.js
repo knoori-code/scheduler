2ac5a3b6fc1bbe778b555704f394b4a23479bf2a
knoori-code <knoori3@gmail.com>
2023-02-21 21:55:24 -0500

    Create DayList and incorporate into App.js file

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,2 +1,23 @@
 import React from "react";
 import DayListItem from "./DayListItem";
+
+export default function DayList({ days, setDay, day}) {
+
+  const parsedDays = days.map((individualDay) => {
+    return (
+      <DayListItem
+        key={individualDay.id}
+        name={individualDay.name}
+        spots={individualDay.spots}
+        selected={individualDay.name === day}
+        setDay={setDay}
+      />
+    )
+  })
+
+  return (
+    <ul>
+      {parsedDays}
+    </ul>
+  );
+} 
