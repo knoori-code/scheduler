3592be9b8a49ce7e15465eb49c27b0b25fc847c6
knoori-code <knoori3@gmail.com>
2023-02-21 20:32:16 -0500

    Add function to modify spots message depending on spots left

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -6,13 +6,25 @@ export default function DayListItem(props) {
 
   const dayClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
-    "day-list__item--full": props.spots === 0
+    "day-list__item--full": props.spots === 0,
   });
+
+  const formatSpots = () => {
+    if (props.spots === 0) {
+      return <h3 className="text--light">no spots remaining</h3>;
+    }
+
+    if (props.spots === 1) {
+      return <h3 className="text--light">1 spot remaining</h3>;
+    }
+
+    return <h3 className="text--light">{props.spots} spots remaining</h3>
+  };
 
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      {formatSpots()}
     </li>
   );
 }
