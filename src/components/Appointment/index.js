a9eef7a0a57e2ae0d19dea33916bdeff59619654
knoori-code <knoori3@gmail.com>
2023-02-25 18:01:25 -0500

    Import custom hook into index.js file for conditional rendering of form compoenent

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,14 +3,29 @@ import "components/Appointment/styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form"
+import useVisualMode from "hooks/useVisualMode";
 
 export default function Appointment(props) {
   const { time, interview, id } = props;
 
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE"
+
+  const { mode, transition, back } = useVisualMode(interview ? SHOW : EMPTY);
+
   return (
     <article className="appointment">
       <Header time={time} />
-      {interview ? <Show student={interview.student} interviewer={interview.interviewer} /> : <Empty />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE && <Form interviewers={[]} onCancel={back} />}
     </article>
   );
 }
