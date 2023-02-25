22ffc3de6b9bd3b9916e82c01b2b5ffee6b8854d
knoori-code <knoori3@gmail.com>
2023-02-25 14:59:08 -0500

    Create useVisualMode hook with transition and back functions

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  const transition = (newMode, replace=false) => {
+    setHistory(prev => {
+      if (replace) {
+        return [...prev.slice(0, prev.length -1), newMode]
+      } else {
+        return [...prev, newMode]
+      }
+    })
+    
+    setMode(newMode);
+  };
+
+  const back = () => {
+    if (history.length <= 1) {
+      return;
+    }
+    const newHistory = [...history];
+    newHistory.pop();
+    setHistory(newHistory);
+    setMode(newHistory[newHistory.length - 1]);
+  };
+
+  return { mode, transition, back };
+}
